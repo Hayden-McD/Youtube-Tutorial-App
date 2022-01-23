0eda615f1251f3e6378c7b1ebd1e9886f70b3c63
Hayden <mcdohr2@student.op.ac.nz>
2022-01-24 11:24:00 +1300

    added code to parse the json response

--- Youtube-Tutorial-App/Model.swift
+++ Youtube-Tutorial-App/Model.swift
@@ -28,8 +28,17 @@ class Model {
                 return
             }
             
-            //parsing the data into video objects
-            
+            do {
+                //parsing the data into video objects
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let response = try decoder.decode(Response.self, from: data!)
+                dump(response)
+            }
+            catch {
+                
+            }
         }
         
         //Kick off the task
