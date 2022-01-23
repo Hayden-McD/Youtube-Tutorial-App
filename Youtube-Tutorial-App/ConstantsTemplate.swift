e4780c429e5360baafbe13d823a9b9c4706e0185
Hayden <mcdohr2@student.op.ac.nz>
2022-01-24 12:36:23 +1300

    Added tableView, custom protocol in model, display video titles in table view

--- Youtube-Tutorial-App/ConstantsTemplate.swift
+++ Youtube-Tutorial-App/ConstantsTemplate.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-//struct Constants {
+struct Constants {
     
-//    static var API_KEY = ""
-//    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
-//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-//}
+    static var API_KEY = ""
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell"
+}
--- Youtube-Tutorial-App/ViewController.swift
+++ Youtube-Tutorial-App/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var model = Model()
+    var videos = [Video]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Set itself as the data source
+        tableView.dataSource = self
+        tableView.delegate = self
+        model.delegate = self
+        
         model.getVideos()
     }
+    
+    // MARK: - Model Methods
+    
+    func videosFetched(_ videos: [Video]) {
+        
+        // Set returned videos to property
+        self.videos = videos
+        
+        // Refresh the tableView
+        tableView.reloadData()
+        
+    }
+        
+    
+    // MARK: - TableViewMethods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        //Configure cell with data
+        let title = self.videos[indexPath.row].title
+        
+        cell.textLabel?.text = title
+        
+        //Return the cell
+        return cell
+    }
+    
+    
 }
